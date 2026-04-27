0c10c8a220eef3504c36e861a72ce6d578afd0f9
Osman Can <cnosman14043@gmail.com>
2026-04-27 20:39:21 +0300

    feat(api): setup prisma orm, define user, repository, and commit models, and run initial migration

---
+++ src/prisma/migrations/20260427173537_init/migration.sql
@@ -0,0 +1,62 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "githubId" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT,
+    "avatarUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Repository" (
+    "id" SERIAL NOT NULL,
+    "githubId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "description" TEXT,
+    "url" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Repository_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Commit" (
+    "id" SERIAL NOT NULL,
+    "sha" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "repositoryId" INTEGER NOT NULL,
+    "authorName" TEXT NOT NULL,
+    "authorEmail" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Commit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_githubId_key" ON "User"("githubId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Repository_githubId_key" ON "Repository"("githubId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Repository_fullName_key" ON "Repository"("fullName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Commit_sha_key" ON "Commit"("sha");
+
+-- AddForeignKey
+ALTER TABLE "Repository" ADD CONSTRAINT "Repository_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Commit" ADD CONSTRAINT "Commit_repositoryId_fkey" FOREIGN KEY ("repositoryId") REFERENCES "Repository"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
